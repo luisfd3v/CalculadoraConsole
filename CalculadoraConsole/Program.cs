5ee9e64678afc799ee444f599e1e1cbf230adb57
luisfd3v <luisfd3v@gmail.com>
2025-08-26 19:50:14 -0300

    refatorando calculadora com switch e tipo double

--- CalculadoraConsole/Program.cs
+++ CalculadoraConsole/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Por favor, digite o primeiro número: ");
+int numero1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Por favor, digite o segundo número: ");
+int numero2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite a operação (+, -, *, /): ");
+string operacao = Console.ReadLine();
+
+double resultado = 0;
+
+switch (operacao)
+{
+    case "+":
+        resultado = numero1 + numero2;
+        break;
+    case "-":
+        resultado = numero1 - numero2;
+        break;
+    case "*":
+        resultado = numero1 * numero2;
+        break;
+    case "/":
+        resultado = numero1 / numero2;
+        break;
+    default:
+        Console.WriteLine("Operação inválida.");
+        return;
+}
+
+Console.WriteLine($"O resultado é: {resultado}");
